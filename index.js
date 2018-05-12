5bc561d7e71181c92137ba22b52ccfb33cf6f14f
MaomiHz <maomihz@gmail.com>
2018-05-13 04:20:31 +0800

    Update ssr

--- index.js
+++ index.js
@@ -8,6 +8,11 @@ const moment = require('moment-timezone');
 const ssurl = 'https://fast.ishadowx.net'
 const nodes = ['usa', 'usb', 'usc', 'jpa', 'jpb',
               'jpc', 'sga', 'sgb', 'sgc']
+const ssrnodes = ['ssra', 'ssrb', 'ssrc']
+
+function b64(s) {
+  return Buffer.from(s).toString('base64');
+}
 
 class Shadowsocks {
   constructor(method, password, hostname, port) {
@@ -19,7 +24,7 @@ class Shadowsocks {
 
   encodess() {
     var url = this.method + ":" + this.password + '@' + this.hostname + ':' + this.port;
-    return 'ss://' + Buffer.from(url).toString('base64');
+    return 'ss://' + b64(url);
   }
 
   encodehtml() {
@@ -34,6 +39,40 @@ class Shadowsocks {
 
 }
 
+class ShadowsocksR extends Shadowsocks {
+  constructor(host,port,protocol,method,obfs,pass) {
+    super(method, pass, host, port);
+    this.obfs = obfs;
+    this.protocol = protocol;
+  }
+
+  encodess() {
+    var url = util.format('%s:%s:%s:%s:%s:%s/',
+      this.hostname, this.port, this.protocol, this.method, this.obfs, b64(this.password));
+    return 'ssr://' + b64(url);
+  }
+
+  encodehtml() {
+    var s = this.encodess();
+    return util.format('<a href="%s">%s</a>', s, s);
+  }
+
+  encodecmd() {
+    return JSON.stringify({
+      'server': this.hostname,
+      'server_port': this.port,
+      'local_address': '127.0.0.1',
+      'local_port': 1080,
+      'password': this.password,
+      'method': this.method,
+      'protocol': this.protocol,
+      'protocol_param': '',
+      'obfs': this.obfs,
+      'obfs_param': ''
+    }, null, 2);
+  }
+}
+
 
 
 
@@ -41,6 +80,7 @@ request(ssurl, function(err, res, body) {
   const $ = cheerio.load(body);
   var d = moment().tz('Asia/Shanghai');
   var servers = [];
+  var subscribe = '';
 
   // Retrieve information
   for (var i = 0; i < nodes.length; i++) {
@@ -55,14 +95,30 @@ request(ssurl, function(err, res, body) {
     servers.push(ss);
   }
 
+  for (var i = 0; i < ssrnodes.length; i++) {
+    var n = ssrnodes[i];
+    var pw = $('#pw' + n).html().trim();
+    var addr = $('#ip' + n).html().trim();
+    var port = $('#port' + n).html().trim();
+    var method = 'aes-256-cfb';
+    var protocol = 'auth_sha1_v4';
+    var obfs = 'tls1.2_ticket_auth';
+
+    var ss = new ShadowsocksR(addr, port, protocol, method, obfs, pw);
+    ss.name = n;
+    servers.push(ss);
+    subscribe += ss.encodess() + '\n';
+  }
+
   // Render template
   fs.readFile('README.mustache', function(err, data) {
     if (err) { throw err; }
-
     var output = mustache.render(data.toString(), {
       'data': servers,
       'update': d.format('YYYY-MM-DD hh:mm:ss')
     });
-    console.log(output);
+    fs.writeFileSync('README.md', output);
+
+    fs.writeFileSync('ssr.txt', b64(subscribe))
   });
 });
